58571c51664e3a548a5be57a853450822f5217ef
MatteoDiMario96 <dm.matteo96@live.it>
2024-08-22 23:12:25 +0200

    Creata una nnv. Creati un router-link con un v-for.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,14 @@
 export default {
     data() {
         return {
-            
+            navLinks:[{
+                "routeName": "AppHome",
+                "linkName": "Home",
+
+            },{
+                "routeName": "ProjectsIndex",
+                "linkName": "Projects",
+            },],
         }
     },
     methods:{
@@ -24,18 +31,12 @@ export default {
                 </button>
                 <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    <li v-for="(link,index) in navLinks" :key="index" class="nav-item">
+                        <router-link :to="{name: link.routeName}" class="nav-link" :class="$route.path === link.path ? 'active' : ''" aria-current="page" href="#">{{link.linkName}}</router-link>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <input class="form-control me-2" type="search" placeholder="Search projects, type, technologies" aria-label="Search">
                     <button class="btn btn-outline-success" type="submit">Search</button>
                 </form>
                 </div>
