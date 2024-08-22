6dfa99f2f6c4f6125c454f3de9281c476f981502
MatteoDiMario96 <dm.matteo96@live.it>
2024-08-22 15:47:28 +0200

    Creato componente. Creato struttura base. Messo un semplice h1 con scritto footer.

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+
+    },
+    created(){
+
+    }
+}
+</script>
+
+<template>
+    <footer>
+        <h1>Footer</h1>
+    </footer>
+</template>
+
+<style scoped>
+
+</style>
