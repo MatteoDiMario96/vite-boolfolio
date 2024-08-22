0b552820d7fd94f3ddaf5df82d0ca2ea231c2caf
MatteoDiMario96 <dm.matteo96@live.it>
2024-08-22 16:04:41 +0200

    importato il component SingleProject creato il method changeCurrentPage() che crea l'emit update:currentPage. Fornito le props: projects che un array required. currentPage che è un number required.
    Nel template messo un tag main e un ul. Poi ho messo il componente e su di esso ho applicato un v-for e fatto iterare sulla'rray pasato tramite props projects.
    E a sua volta ho passato tramite props il singolo project al suo componente figlio i parametri singoli che mi interssevano.
    Applicato un tag a con la forma di un btn, e collegato una funzione al click changeCurrentPage() che appunto con l'emit va ad attivare la funzione updatePage che va ad incrementare la currentPage e a richiamare la chiamata API.

---
+++ src/components/AppMain.vue
@@ -0,0 +1,51 @@
+<script>
+import SingleProject from './SingleProject.vue';
+export default {
+    components:{
+        SingleProject,
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+        changeCurrentPage(){
+            this.$emit('update:currentPage');
+        },
+    },
+    created(){
+        
+    },
+    props:{
+        projects:{
+            typeof: Array,
+            required: true,
+        },
+        currentPage: {
+            type: Number,
+            required: true,
+        },
+    },
+}
+</script>
+
+<template>
+    <main>
+        <ul>
+            <SingleProject v-for="project in projects"  :key="project.id"
+            :name="project.name"
+            :project_created_at="project.project_created_at"
+            :type="project.type"
+            :technologies="project.technologies"
+            />
+        </ul>
+
+        <a href="#" class="btn btn-primary" @click.prevent="changeCurrentPage()">Load More</a>
+    </main>
+
+</template>
+
+<style scoped>
+
+</style>
