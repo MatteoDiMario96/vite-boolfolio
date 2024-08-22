638741e5195360c755a5ba30101213478e59689f
MatteoDiMario96 <dm.matteo96@live.it>
2024-08-22 23:13:18 +0200

    Creata la pagina 404

---
+++ src/pages/Error404.vue
@@ -0,0 +1,27 @@
+<script>
+
+export default {
+    name:   "404-not-found",
+    components:{
+        
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+        
+    },
+    created(){
+    }
+}
+</script>
+
+<template>
+    <h1>404 NOT FOUND</h1>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
