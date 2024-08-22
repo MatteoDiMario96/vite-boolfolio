f0334a4565093319117dbbd053128f2f07907755
MatteoDiMario96 <dm.matteo96@live.it>
2024-08-22 16:56:54 +0200

    Creato un footer molto stupido.

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -15,8 +15,22 @@ export default {
 </script>
 
 <template>
-    <footer>
-        <h1>Footer</h1>
+    <footer class="bg-dark text-white text-center py-3 my-5">
+        <div class="container">
+            <p class="mb-0">© 2024 MyProject. All Rights Reserved.</p>
+            <p class="mb-0">Powered by Vue.js & Bootstrap</p>
+            <ul class="list-inline mt-2">
+                <li class="list-inline-item">
+                    <a href="#" class="text-white">Privacy Policy</a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="#" class="text-white">Terms of Service</a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="#" class="text-white">Contact Us</a>
+                </li>
+            </ul>
+        </div>
     </footer>
 </template>
 
