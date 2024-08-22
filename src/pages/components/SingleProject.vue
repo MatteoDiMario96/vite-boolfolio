b56c14d033d284fc8c0e2876752569a7a17a2879
MatteoDiMario96 <dm.matteo96@live.it>
2024-08-22 23:11:42 +0200

    Fatte arrivare le props e nel tempalte fatto visuallizzare i dati necessari.

---
+++ src/pages/components/SingleProject.vue
@@ -0,0 +1,64 @@
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+        
+    },
+    created(){
+        
+    },
+    props:{
+        id:{
+            type: Number,
+            required: true,
+        },
+        name:{
+            type: String,
+            required: true,
+        },
+        project_created_at:{
+            type: String,
+            required: true,
+        },
+        type:{
+            type: Object,
+            required: true,
+        },
+        technologies:{
+            type: Array,
+            required: true,
+        },
+    },
+}
+</script>
+
+<template>
+    <li>
+        <div class="card text-center mt-4">
+            <h3 class="card-header">{{ name }}</h3>
+            <div class="card-body">
+                <div  class="d-flex justify-content-center">
+                    <span v-for="(technology,index) in technologies" :key="technology.id">
+                        <h5 v-if="index < technologies.length - 1" class="card-title">{{technology.name}}  |-|&nbsp;   </h5>
+                        <h5 v-else-if="index < technologies.length" class="card-title"> {{technology.name}} </h5>
+                    </span>
+                </div>
+                <h5 class="card-title">
+                    <em>{{ type.name }}</em>
+
+                </h5>
+                <p class="card-text">{{ project_created_at }}</p>
+                <router-link :to="{name: 'ProjectsShow', params:{id: id}}" class="btn btn-primary ">Vedi dettagli</router-link>
+            </div>  
+        </div>
+    </li>
+    
+</template>
+
+<style scoped>
+
+</style>
