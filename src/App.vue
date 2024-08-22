8a0b1a6f2650188e8a9e07a4b8f0a7104d4dde43
MatteoDiMario96 <dm.matteo96@live.it>
2024-08-22 23:08:54 +0200

    Tolto il componente AppMain e messo il router-view. Tolta la chiamata API con i sue relativi metodi e variabili.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
 
 import axios from 'axios';
@@ -9,41 +8,18 @@ import axios from 'axios';
 export default {
     components:{
       AppHeader,
-      AppMain,
       AppFooter,
     },
     data() {
         return {
-            projects: [],
-            currentPage: 1,
+            
         }
     },
     methods:{
-      getProjectsPaginate(){
-            axios.get('http://127.0.0.1:8000/api/projects', {
-                params: {
-                    page: this.currentPage,
-                }
-            })
-            .then((response) => {
-                console.log(response.data.results.data);
-                this.projects = [...this.projects, ...response.data.results.data];
-
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-            .finally(function () {
-                // always executed
-            });  
-        },
-        updatePage(){
-            this.currentPage += 1;
-            this.getProjectsPaginate();
-        },
+      
     },
     created(){
-      this.getProjectsPaginate();
+      
     }
 }
 </script>
@@ -51,13 +27,7 @@ export default {
 <template>
     <div class="container">
         <AppHeader/>
-
-        <AppMain
-        :projects="projects"
-        :currentPage="currentPage"
-        @update:currentPage="updatePage"
-        />
-
+        <router-view></router-view>
         <AppFooter/>
     </div>
 </template>
