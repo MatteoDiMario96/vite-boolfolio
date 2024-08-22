1772c049c3bd1df62f5cda1fb604b10c9e549b5c
MatteoDiMario96 <dm.matteo96@live.it>
2024-08-22 15:46:25 +0200

    Importato i 3 componenti AppHeader, AppMain, AppFooter. Create due variabili una per i progetti che viene popolata dalla chiamata api tramite axios. una per la pagina corrente sempre per la chiamata api che standard è impostata a a a 1. Nei methods ho creato una funzione che mi va a a prendere tramite chiamata get all'indirizzo da me fornito creato nel progetto laravel many-to-many con parametro della variabile della pagina corrente. Se ha successo aggiungi all'array projects, i data ricevuti dalla'pi. Cosi da permettere un lazy load. Poi creato un method updatePae che appunto fa aggiungere alla variabile currentPage 1 e chiamata la funzione getProjectsPaginate.
    Nel template chiamo i componenti
    Appheader.
    AppMain, con la props currentPage e l'emit update:currentPage.
    AppFooter.
    
    Usato il foglio di stile general.scss

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+import axios from 'axios';
+
+
+export default {
+    components:{
+      AppHeader,
+      AppMain,
+      AppFooter,
+    },
+    data() {
+        return {
+            projects: [],
+            currentPage: 1,
+        }
+    },
+    methods:{
+      getProjectsPaginate(){
+            axios.get('http://127.0.0.1:8000/api/projects', {
+                params: {
+                    page: this.currentPage,
+                }
+            })
+            .then((response) => {
+                console.log(response.data.results.data);
+                this.projects = [...this.projects, ...response.data.results.data];
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });  
+        },
+        updatePage(){
+            this.currentPage += 1;
+            this.getProjectsPaginate();
+        },
+    },
+    created(){
+      this.getProjectsPaginate();
+    }
+}
+</script>
+
+<template>
+    <AppHeader/>
+
+    <AppMain
+    :projects="projects"
+    :currentPage="currentPage"
+    @update:currentPage="updatePage"
+    />
+
+    <AppFooter/>
+
+</template>
+
+<style lang="scss">
+  @use './scss/general.scss'
+</style>
