9b72a724cbf68fafc9a056c43ef9bcc935d61578
MatteoDiMario96 <dm.matteo96@live.it>
2024-08-22 23:13:04 +0200

    Creata la pagina Home.

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,30 @@
+ <script>
+
+export default {
+    name:   "AppHome",
+    components:{
+        
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+      
+    },
+    created(){
+      
+    }
+}
+</script>
+
+<template>
+    <h1>
+        Sono la Home
+    </h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
